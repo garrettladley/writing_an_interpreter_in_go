86d1d908095db542d8d54d86c7d19a13629b4f9b
garrettladley <ladley.g@northeastern.edu>
2024-03-10 20:53:35 -0400

    chapter 2 completed

--- src/monkey/parser/parser.go
+++ src/monkey/parser/parser.go
@@ -2,19 +2,47 @@ package parser
 
 import (
 	"fmt"
+	"strconv"
+
 	"monkey/ast"
 	"monkey/lexer"
 	"monkey/token"
-	"strconv"
+)
+
+const (
+	_ int = iota
+	LOWEST
+	EQUALS      // ==
+	LESSGREATER // > or <
+	SUM         // +
+	PRODUCT     // *
+	PREFIX      // -X or !X
+	CALL        // myFunction(X)
+)
+
+var precedences = map[token.TokenType]int{
+	token.EQ:       EQUALS,
+	token.NOT_EQ:   EQUALS,
+	token.LT:       LESSGREATER,
+	token.GT:       LESSGREATER,
+	token.PLUS:     SUM,
+	token.MINUS:    SUM,
+	token.SLASH:    PRODUCT,
+	token.ASTERISK: PRODUCT,
+	token.LPAREN:   CALL,
+}
+
+type (
+	prefixParseFn func() ast.Expression
+	infixParseFn  func(ast.Expression) ast.Expression
 )
 
 type Parser struct {
-	l *lexer.Lexer
+	l      *lexer.Lexer
+	errors []string
 
 	curToken  token.Token
 	peekToken token.Token
-
-	errors []string
 
 	prefixParseFns map[token.TokenType]prefixParseFn
 	infixParseFns  map[token.TokenType]infixParseFn
@@ -26,14 +54,16 @@ func New(l *lexer.Lexer) *Parser {
 		errors: []string{},
 	}
 
-	p.nextToken()
-	p.nextToken()
-
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
 	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 	p.registerPrefix(token.BANG, p.parsePrefixExpression)
 	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
+	p.registerPrefix(token.TRUE, p.parseBoolean)
+	p.registerPrefix(token.FALSE, p.parseBoolean)
+	p.registerPrefix(token.LPAREN, p.parseGroupedExpression)
+	p.registerPrefix(token.IF, p.parseIfExpression)
+	p.registerPrefix(token.FUNCTION, p.parseFunctionLiteral)
 
 	p.infixParseFns = make(map[token.TokenType]infixParseFn)
 	p.registerInfix(token.PLUS, p.parseInfixExpression)
@@ -45,7 +75,35 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerInfix(token.LT, p.parseInfixExpression)
 	p.registerInfix(token.GT, p.parseInfixExpression)
 
+	p.registerInfix(token.LPAREN, p.parseCallExpression)
+
+	p.nextToken()
+	p.nextToken()
+
 	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		p.peekError(t)
+		return false
+	}
 }
 
 func (p *Parser) Errors() []string {
@@ -53,13 +111,14 @@ func (p *Parser) Errors() []string {
 }
 
 func (p *Parser) peekError(t token.TokenType) {
-	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+		t, p.peekToken.Type)
 	p.errors = append(p.errors, msg)
 }
 
-func (p *Parser) nextToken() {
-	p.curToken = p.peekToken
-	p.peekToken = p.l.NextToken()
+func (p *Parser) noPrefixParseFnError(t token.TokenType) {
+	msg := fmt.Sprintf("no prefix parse function for %s found", t)
+	p.errors = append(p.errors, msg)
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
@@ -68,20 +127,35 @@ func (p *Parser) ParseProgram() *ast.Program {
 
 	for !p.curTokenIs(token.EOF) {
 		stmt := p.parseStatement()
-		program.Statements = append(program.Statements, stmt)
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
 		p.nextToken()
 	}
+
 	return program
 }
 
 func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
-		return p.parseLetStatement()
+		stmt := p.parseLetStatement()
+		if stmt == nil {
+			return nil
+		}
+		return stmt
 	case token.RETURN:
-		return p.parseReturnStatement()
+		stmt := p.parseReturnStatement()
+		if stmt == nil {
+			return nil
+		}
+		return stmt
 	default:
-		return p.parseExpressionStatement()
+		stmt := p.parseExpressionStatement()
+		if stmt == nil {
+			return nil
+		}
+		return stmt
 	}
 }
 
@@ -98,7 +172,11 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 		return nil
 	}
 
-	for !p.curTokenIs(token.SEMICOLON) {
+	p.nextToken()
+
+	stmt.Value = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
 
@@ -110,7 +188,9 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 
 	p.nextToken()
 
-	for !p.curTokenIs(token.SEMICOLON) {
+	stmt.ReturnValue = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
 
@@ -129,23 +209,16 @@ func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
 	return stmt
 }
 
-func (p *Parser) noPrefixParseFnError(t token.TokenType) {
-	p.errors = append(p.errors, fmt.Sprintf("no prefix parse function for %s found", t))
-}
-
 func (p *Parser) parseExpression(precedence int) ast.Expression {
 	prefix := p.prefixParseFns[p.curToken.Type]
-
 	if prefix == nil {
 		p.noPrefixParseFnError(p.curToken.Type)
 		return nil
 	}
-
 	leftExp := prefix()
 
 	for !p.peekTokenIs(token.SEMICOLON) && precedence < p.peekPrecedence() {
 		infix := p.infixParseFns[p.peekToken.Type]
-
 		if infix == nil {
 			return leftExp
 		}
@@ -158,6 +231,22 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 	return leftExp
 }
 
+func (p *Parser) peekPrecedence() int {
+	if p, ok := precedences[p.peekToken.Type]; ok {
+		return p
+	}
+
+	return LOWEST
+}
+
+func (p *Parser) curPrecedence() int {
+	if p, ok := precedences[p.curToken.Type]; ok {
+		return p
+	}
+
+	return LOWEST
+}
+
 func (p *Parser) parseIdentifier() ast.Expression {
 	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
 }
@@ -166,10 +255,9 @@ func (p *Parser) parseIntegerLiteral() ast.Expression {
 	lit := &ast.IntegerLiteral{Token: p.curToken}
 
 	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
-
 	if err != nil {
-		p.errors = append(p.errors, fmt.Sprintf("could not parse %q as integer", p.curToken.Literal))
-
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
 		return nil
 	}
 
@@ -205,28 +293,146 @@ func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
 	return expression
 }
 
-func (p *Parser) curTokenIs(t token.TokenType) bool {
-	return p.curToken.Type == t
+func (p *Parser) parseBoolean() ast.Expression {
+	return &ast.Boolean{Token: p.curToken, Value: p.curTokenIs(token.TRUE)}
 }
 
-func (p *Parser) peekTokenIs(t token.TokenType) bool {
-	return p.peekToken.Type == t
-}
+func (p *Parser) parseGroupedExpression() ast.Expression {
+	p.nextToken()
 
-func (p *Parser) expectPeek(t token.TokenType) bool {
-	if p.peekTokenIs(t) {
-		p.nextToken()
-		return true
-	} else {
-		p.peekError(t)
-		return false
+	exp := p.parseExpression(LOWEST)
+
+	if !p.expectPeek(token.RPAREN) {
+		return nil
 	}
+
+	return exp
 }
 
-type (
-	prefixParseFn func() ast.Expression
-	infixParseFn  func(ast.Expression) ast.Expression
-)
+func (p *Parser) parseIfExpression() ast.Expression {
+	expression := &ast.IfExpression{Token: p.curToken}
+
+	if !p.expectPeek(token.LPAREN) {
+		return nil
+	}
+
+	p.nextToken()
+	expression.Condition = p.parseExpression(LOWEST)
+
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+
+	if !p.expectPeek(token.LBRACE) {
+		return nil
+	}
+
+	expression.Consequence = p.parseBlockStatement()
+
+	if p.peekTokenIs(token.ELSE) {
+		p.nextToken()
+
+		if !p.expectPeek(token.LBRACE) {
+			return nil
+		}
+
+		expression.Alternative = p.parseBlockStatement()
+	}
+
+	return expression
+}
+
+func (p *Parser) parseBlockStatement() *ast.BlockStatement {
+	block := &ast.BlockStatement{Token: p.curToken}
+	block.Statements = []ast.Statement{}
+
+	p.nextToken()
+
+	for !p.curTokenIs(token.RBRACE) && !p.curTokenIs(token.EOF) {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			block.Statements = append(block.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return block
+}
+
+func (p *Parser) parseFunctionLiteral() ast.Expression {
+	lit := &ast.FunctionLiteral{Token: p.curToken}
+
+	if !p.expectPeek(token.LPAREN) {
+		return nil
+	}
+
+	lit.Parameters = p.parseFunctionParameters()
+
+	if !p.expectPeek(token.LBRACE) {
+		return nil
+	}
+
+	lit.Body = p.parseBlockStatement()
+
+	return lit
+}
+
+func (p *Parser) parseFunctionParameters() []*ast.Identifier {
+	identifiers := []*ast.Identifier{}
+
+	if p.peekTokenIs(token.RPAREN) {
+		p.nextToken()
+		return identifiers
+	}
+
+	p.nextToken()
+
+	ident := &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+	identifiers = append(identifiers, ident)
+
+	for p.peekTokenIs(token.COMMA) {
+		p.nextToken()
+		p.nextToken()
+		ident := &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+		identifiers = append(identifiers, ident)
+	}
+
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+
+	return identifiers
+}
+
+func (p *Parser) parseCallExpression(function ast.Expression) ast.Expression {
+	exp := &ast.CallExpression{Token: p.curToken, Function: function}
+	exp.Arguments = p.parseCallArguments()
+	return exp
+}
+
+func (p *Parser) parseCallArguments() []ast.Expression {
+	args := []ast.Expression{}
+
+	if p.peekTokenIs(token.RPAREN) {
+		p.nextToken()
+		return args
+	}
+
+	p.nextToken()
+	args = append(args, p.parseExpression(LOWEST))
+
+	for p.peekTokenIs(token.COMMA) {
+		p.nextToken()
+		p.nextToken()
+		args = append(args, p.parseExpression(LOWEST))
+	}
+
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+
+	return args
+}
 
 func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
 	p.prefixParseFns[tokenType] = fn
@@ -234,42 +440,4 @@ func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
 
 func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
 	p.infixParseFns[tokenType] = fn
-}
-
-const (
-	_ int = iota
-	LOWEST
-	EQUALS      // ==
-	LESSGREATER // > or <
-	SUM         // +
-	PRODUCT     // *
-	PREFIX      // -X or !X
-	CALL        // myFunction(X)
-)
-
-var precedences = map[token.TokenType]int{
-	token.EQ:       EQUALS,
-	token.NOT_EQ:   EQUALS,
-	token.LT:       LESSGREATER,
-	token.GT:       LESSGREATER,
-	token.PLUS:     SUM,
-	token.MINUS:    SUM,
-	token.SLASH:    PRODUCT,
-	token.ASTERISK: PRODUCT,
-}
-
-func (p *Parser) peekPrecedence() int {
-	if p, ok := precedences[p.peekToken.Type]; ok {
-		return p
-	}
-
-	return LOWEST
-}
-
-func (p *Parser) curPrecedence() int {
-	if p, ok := precedences[p.curToken.Type]; ok {
-		return p
-	}
-
-	return LOWEST
 }
--- src/monkey/parser/parser_test.go
+++ src/monkey/parser/parser_test.go
@@ -1,69 +1,77 @@
 package parser
 
 import (
-	"monkey/ast"
-	"monkey/lexer"
 	"strconv"
 	"testing"
+
+	"monkey/ast"
+	"monkey/lexer"
 
 	"github.com/huandu/go-assert"
 )
 
 func TestLetStatements(t *testing.T) {
 	assert := assert.New(t)
-	input := `
-let x = 5;
-let y = 10;
-let foobar = 838383;
-`
-
-	l := lexer.New(input)
-	p := New(l)
-
-	program := p.ParseProgram()
-	checkParserErrors(assert, p)
-
-	assert.NotEqual(program, nil)
-
-	assert.Equal(len(program.Statements), 3)
 
 	tests := []struct {
+		input              string
 		expectedIdentifier string
+		expectedValue      interface{}
 	}{
-		{"x"},
-		{"y"},
-		{"foobar"},
+		{"let x = 5;", "x", 5},
+		{"let y = true;", "y", true},
+		{"let foobar = y;", "foobar", "y"},
 	}
 
-	for i, tt := range tests {
-		assert.Assert(testLetStatement(assert, program.Statements[i], tt.expectedIdentifier))
+	for _, tt := range tests {
+		l := lexer.New(tt.input)
+		p := New(l)
+
+		program := p.ParseProgram()
+		checkParserErrors(assert, p)
+
+		assert.Equal(len(program.Statements), 1)
+
+		stmt := program.Statements[0]
+
+		assert.Assert(testLetStatement(assert, stmt, tt.expectedIdentifier))
+
+		val := stmt.(*ast.LetStatement).Value
+
+		assert.Assert(testLiteralExpression(assert, val, tt.expectedValue))
 	}
 }
 
 func TestReturnStatements(t *testing.T) {
 	assert := assert.New(t)
-	input := `
-return 5;
-return 10;
-return 993322;
-`
 
-	l := lexer.New(input)
-	p := New(l)
+	tests := []struct {
+		input         string
+		expectedValue interface{}
+	}{
+		{"return 5;", 5},
+		{"return true;", true},
+		{"return foobar;", "foobar"},
+	}
 
-	program := p.ParseProgram()
-	checkParserErrors(assert, p)
+	for _, tt := range tests {
+		l := lexer.New(tt.input)
+		p := New(l)
 
-	assert.NotEqual(program, nil)
+		program := p.ParseProgram()
+		checkParserErrors(assert, p)
 
-	assert.Equal(len(program.Statements), 3)
+		assert.Equal(len(program.Statements), 1)
 
-	for _, stmt := range program.Statements {
+		stmt := program.Statements[0]
+
 		returnStmt, ok := stmt.(*ast.ReturnStatement)
 
 		assert.Assert(ok)
 
 		assert.Equal(returnStmt.TokenLiteral(), "return")
+
+		assert.Assert(testLiteralExpression(assert, returnStmt.ReturnValue, tt.expectedValue))
 	}
 }
 
@@ -123,10 +131,14 @@ func TestParsingPrefixExpressions(t *testing.T) {
 	prefixTests := []struct {
 		input    string
 		operator string
-		value    int64
+		value    interface{}
 	}{
 		{"!5;", "!", 5},
 		{"-15;", "-", 15},
+		{"!foobar;", "!", "foobar"},
+		{"-foobar;", "-", "foobar"},
+		{"!true;", "!", true},
+		{"!false;", "!", false},
 	}
 
 	for _, tt := range prefixTests {
@@ -147,7 +159,7 @@ func TestParsingPrefixExpressions(t *testing.T) {
 
 		assert.Equal(exp.Operator, tt.operator)
 
-		assert.Assert(testIntegerLiteral(assert, exp.Right, tt.value))
+		assert.Assert(testLiteralExpression(assert, exp.Right, tt.value))
 	}
 }
 
@@ -155,10 +167,10 @@ func TestParsingInfixExpressions(t *testing.T) {
 	assert := assert.New(t)
 
 	infixTests := []struct {
-		input    string
-		leftVal  int64
-		operator string
-		rightVal int64
+		input      string
+		leftValue  interface{}
+		operator   string
+		rightValue interface{}
 	}{
 		{"5 + 5;", 5, "+", 5},
 		{"5 - 5;", 5, "-", 5},
@@ -168,6 +180,17 @@ func TestParsingInfixExpressions(t *testing.T) {
 		{"5 < 5;", 5, "<", 5},
 		{"5 == 5;", 5, "==", 5},
 		{"5 != 5;", 5, "!=", 5},
+		{"foobar + barfoo;", "foobar", "+", "barfoo"},
+		{"foobar - barfoo;", "foobar", "-", "barfoo"},
+		{"foobar * barfoo;", "foobar", "*", "barfoo"},
+		{"foobar / barfoo;", "foobar", "/", "barfoo"},
+		{"foobar > barfoo;", "foobar", ">", "barfoo"},
+		{"foobar < barfoo;", "foobar", "<", "barfoo"},
+		{"foobar == barfoo;", "foobar", "==", "barfoo"},
+		{"foobar != barfoo;", "foobar", "!=", "barfoo"},
+		{"true == true", true, "==", true},
+		{"true != false", true, "!=", false},
+		{"false == false", false, "==", false},
 	}
 
 	for _, tt := range infixTests {
@@ -182,15 +205,7 @@ func TestParsingInfixExpressions(t *testing.T) {
 
 		assert.Assert(ok)
 
-		exp, ok := stmt.Expression.(*ast.InfixExpression)
-
-		assert.Assert(ok)
-
-		assert.Assert(testIntegerLiteral(assert, exp.Left, tt.leftVal))
-
-		assert.Equal(exp.Operator, tt.operator)
-
-		assert.Assert(testIntegerLiteral(assert, exp.Right, tt.rightVal))
+		assert.Assert(testInfixExpression(assert, stmt.Expression, tt.leftValue, tt.operator, tt.rightValue))
 	}
 }
 
@@ -201,18 +216,106 @@ func TestOperatorPrecedenceParsing(t *testing.T) {
 		input    string
 		expected string
 	}{
-		{"-a * b", "((-a) * b)"},
-		{"!-a", "(!(-a))"},
-		{"a + b + c", "((a + b) + c)"},
-		{"a + b - c", "((a + b) - c)"},
-		{"a * b * c", "((a * b) * c)"},
-		{"a * b / c", "((a * b) / c)"},
-		{"a + b / c", "(a + (b / c))"},
-		{"a + b * c + d / e - f", "(((a + (b * c)) + (d / e)) - f)"},
-		{"3 + 4; -5 * 5", "(3 + 4)((-5) * 5)"},
-		{"5 > 4 == 3 < 4", "((5 > 4) == (3 < 4))"},
-		{"5 < 4 != 3 > 4", "((5 < 4) != (3 > 4))"},
-		{"3 + 4 * 5 == 3 * 1 + 4 * 5", "((3 + (4 * 5)) == ((3 * 1) + (4 * 5)))"},
+		{
+			"-a * b",
+			"((-a) * b)",
+		},
+		{
+			"!-a",
+			"(!(-a))",
+		},
+		{
+			"a + b + c",
+			"((a + b) + c)",
+		},
+		{
+			"a + b - c",
+			"((a + b) - c)",
+		},
+		{
+			"a * b * c",
+			"((a * b) * c)",
+		},
+		{
+			"a * b / c",
+			"((a * b) / c)",
+		},
+		{
+			"a + b / c",
+			"(a + (b / c))",
+		},
+		{
+			"a + b * c + d / e - f",
+			"(((a + (b * c)) + (d / e)) - f)",
+		},
+		{
+			"3 + 4; -5 * 5",
+			"(3 + 4)((-5) * 5)",
+		},
+		{
+			"5 > 4 == 3 < 4",
+			"((5 > 4) == (3 < 4))",
+		},
+		{
+			"5 < 4 != 3 > 4",
+			"((5 < 4) != (3 > 4))",
+		},
+		{
+			"3 + 4 * 5 == 3 * 1 + 4 * 5",
+			"((3 + (4 * 5)) == ((3 * 1) + (4 * 5)))",
+		},
+		{
+			"true",
+			"true",
+		},
+		{
+			"false",
+			"false",
+		},
+		{
+			"3 > 5 == false",
+			"((3 > 5) == false)",
+		},
+		{
+			"3 < 5 == true",
+			"((3 < 5) == true)",
+		},
+		{
+			"1 + (2 + 3) + 4",
+			"((1 + (2 + 3)) + 4)",
+		},
+		{
+			"(5 + 5) * 2",
+			"((5 + 5) * 2)",
+		},
+		{
+			"2 / (5 + 5)",
+			"(2 / (5 + 5))",
+		},
+		{
+			"(5 + 5) * 2 * (5 + 5)",
+			"(((5 + 5) * 2) * (5 + 5))",
+		},
+		{
+			"-(5 + 5)",
+			"(-(5 + 5))",
+		},
+		{
+			"!(true == true)",
+			"(!(true == true))",
+		},
+		{
+			"a + add(b * c) + d",
+			"((a + add((b * c))) + d)",
+		},
+		{
+			"add(a, b, 1, 2 * 3, 4 + 5, add(6, 7 * 8))",
+			"add(a, b, 1, (2 * 3), (4 + 5), add(6, (7 * 8)))",
+		},
+		{
+			"add(a + b + c * d / f + g)",
+			"add((((a + b) + ((c * d) / f)) + g))",
+		},
 	}
 
 	for _, tt := range tests {
@@ -222,6 +325,252 @@ func TestOperatorPrecedenceParsing(t *testing.T) {
 		checkParserErrors(assert, p)
 
 		assert.Equal(program.String(), tt.expected)
+	}
+}
+
+func TestBooleanExpression(t *testing.T) {
+	assert := assert.New(t)
+
+	tests := []struct {
+		input           string
+		expectedBoolean bool
+	}{
+		{"true;", true},
+		{"false;", false},
+	}
+
+	for _, tt := range tests {
+		l := lexer.New(tt.input)
+		p := New(l)
+		program := p.ParseProgram()
+		checkParserErrors(assert, p)
+
+		assert.Equal(len(program.Statements), 1)
+
+		stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+
+		assert.Assert(ok)
+
+		boolean, ok := stmt.Expression.(*ast.Boolean)
+
+		assert.Assert(ok)
+
+		assert.Equal(boolean.Value, tt.expectedBoolean)
+	}
+}
+
+func TestIfExpression(t *testing.T) {
+	assert := assert.New(t)
+
+	input := "if (x < y) { x }"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(assert, p)
+
+	assert.Equal(len(program.Statements), 1)
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+
+	assert.Assert(ok)
+
+	exp, ok := stmt.Expression.(*ast.IfExpression)
+
+	assert.Assert(ok)
+
+	assert.Assert(testInfixExpression(assert, exp.Condition, "x", "<", "y"))
+
+	assert.Equal(len(exp.Consequence.Statements), 1)
+
+	consequence, ok := exp.Consequence.Statements[0].(*ast.ExpressionStatement)
+
+	assert.Assert(ok)
+
+	assert.Assert(testIdentifier(assert, consequence.Expression, "x"))
+
+	assert.Assert(exp.Alternative == nil)
+}
+
+func TestIfElseExpression(t *testing.T) {
+	assert := assert.New(t)
+
+	input := "if (x < y) { x } else { y }"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(assert, p)
+
+	assert.Equal(len(program.Statements), 1)
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+
+	assert.Assert(ok)
+
+	exp, ok := stmt.Expression.(*ast.IfExpression)
+
+	assert.Assert(ok)
+
+	assert.Assert(testInfixExpression(assert, exp.Condition, "x", "<", "y"))
+
+	assert.Equal(len(exp.Consequence.Statements), 1)
+
+	consequence, ok := exp.Consequence.Statements[0].(*ast.ExpressionStatement)
+
+	assert.Assert(ok)
+
+	assert.Assert(testIdentifier(assert, consequence.Expression, "x"))
+
+	assert.Equal(len(exp.Alternative.Statements), 1)
+
+	alternative, ok := exp.Alternative.Statements[0].(*ast.ExpressionStatement)
+
+	assert.Assert(ok)
+
+	assert.Assert(testIdentifier(assert, alternative.Expression, "y"))
+}
+
+func TestFunctionLiteralParsing(t *testing.T) {
+	assert := assert.New(t)
+
+	input := "fn(x, y) { x + y; }"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(assert, p)
+
+	assert.Equal(len(program.Statements), 1)
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+
+	assert.Assert(ok)
+
+	function, ok := stmt.Expression.(*ast.FunctionLiteral)
+
+	assert.Assert(ok)
+
+	assert.Equal(len(function.Parameters), 2)
+
+	assert.Assert(testLiteralExpression(assert, function.Parameters[0], "x"))
+
+	assert.Assert(testLiteralExpression(assert, function.Parameters[1], "y"))
+
+	assert.Equal(len(function.Body.Statements), 1)
+
+	bodyStmt, ok := function.Body.Statements[0].(*ast.ExpressionStatement)
+
+	assert.Assert(ok)
+
+	assert.Assert(testInfixExpression(assert, bodyStmt.Expression, "x", "+", "y"))
+}
+
+func TestFunctionParameterParsing(t *testing.T) {
+	assert := assert.New(t)
+
+	tests := []struct {
+		input    string
+		expected []string
+	}{
+		{"fn() {};", []string{}},
+		{"fn(x) {};", []string{"x"}},
+		{"fn(x, y, z) {};", []string{"x", "y", "z"}},
+	}
+
+	for _, tt := range tests {
+		l := lexer.New(tt.input)
+		p := New(l)
+		program := p.ParseProgram()
+		checkParserErrors(assert, p)
+
+		stmt := program.Statements[0].(*ast.ExpressionStatement)
+
+		function := stmt.Expression.(*ast.FunctionLiteral)
+
+		assert.Equal(len(function.Parameters), len(tt.expected))
+
+		for i, ident := range tt.expected {
+			testLiteralExpression(assert, function.Parameters[i], ident)
+		}
+	}
+}
+
+func TestCallExpressionParsing(t *testing.T) {
+	assert := assert.New(t)
+
+	input := "add(1, 2 * 3, 4 + 5);"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(assert, p)
+
+	assert.Equal(len(program.Statements), 1)
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+
+	assert.Assert(ok)
+
+	exp, ok := stmt.Expression.(*ast.CallExpression)
+
+	assert.Assert(ok)
+
+	assert.Assert(testIdentifier(assert, exp.Function, "add"))
+
+	assert.Equal(len(exp.Arguments), 3)
+
+	assert.Assert(testLiteralExpression(assert, exp.Arguments[0], 1))
+
+	assert.Assert(testInfixExpression(assert, exp.Arguments[1], 2, "*", 3))
+
+	assert.Assert(testInfixExpression(assert, exp.Arguments[2], 4, "+", 5))
+}
+
+func TestCallExpressionParameterParsing(t *testing.T) {
+	assert := assert.New(t)
+
+	tests := []struct {
+		input         string
+		expectedIdent string
+		expectedArgs  []string
+	}{
+		{
+			input:         "add();",
+			expectedIdent: "add",
+			expectedArgs:  []string{},
+		},
+		{
+			input:         "add(1);",
+			expectedIdent: "add",
+			expectedArgs:  []string{"1"},
+		},
+		{
+			input:         "add(1, 2 * 3, 4 + 5);",
+			expectedIdent: "add",
+			expectedArgs:  []string{"1", "(2 * 3)", "(4 + 5)"},
+		},
+	}
+
+	for _, tt := range tests {
+		l := lexer.New(tt.input)
+		p := New(l)
+		program := p.ParseProgram()
+		checkParserErrors(assert, p)
+
+		stmt := program.Statements[0].(*ast.ExpressionStatement)
+
+		exp, ok := stmt.Expression.(*ast.CallExpression)
+
+		assert.Assert(ok)
+
+		assert.Assert(testIdentifier(assert, exp.Function, tt.expectedIdent))
+
+		assert.Equal(len(exp.Arguments), len(tt.expectedArgs))
+
+		for i, arg := range tt.expectedArgs {
+			assert.Equal(exp.Arguments[i].String(), arg)
+		}
 	}
 }
 
@@ -259,6 +608,61 @@ func testIntegerLiteral(assert *assert.A, il ast.Expression, value int64) bool {
 
 	assert.Equal(integ.Value, value)
 	assert.Equal(integ.TokenLiteral(), strconv.Itoa(int(value)))
+
+	return true
+}
+
+func testIdentifier(assert *assert.A, exp ast.Expression, value string) bool {
+	ident, ok := exp.(*ast.Identifier)
+
+	assert.Assert(ok)
+
+	assert.Equal(ident.Value, value)
+
+	assert.Equal(ident.TokenLiteral(), value)
+
+	return true
+}
+
+func testBooleanLiteral(assert *assert.A, exp ast.Expression, value bool) bool {
+	bo, ok := exp.(*ast.Boolean)
+
+	assert.Assert(ok)
+
+	assert.Equal(bo.Value, value)
+
+	assert.Equal(bo.TokenLiteral(), strconv.FormatBool(value))
+
+	return true
+}
+
+func testLiteralExpression(assert *assert.A, exp ast.Expression, expected interface{}) bool {
+	switch v := expected.(type) {
+	case int:
+		return testIntegerLiteral(assert, exp, int64(v))
+	case int64:
+		return testIntegerLiteral(assert, exp, v)
+	case string:
+		return testIdentifier(assert, exp, v)
+	case bool:
+		return testBooleanLiteral(assert, exp, v)
+	}
+
+	assert.Errorf("type of exp not handled. got=%T", exp)
+
+	return false
+}
+
+func testInfixExpression(assert *assert.A, exp ast.Expression, left interface{}, operator string, right interface{}) bool {
+	opExp, ok := exp.(*ast.InfixExpression)
+
+	assert.Assert(ok)
+
+	assert.Assert(testLiteralExpression(assert, opExp.Left, left))
+
+	assert.Equal(opExp.Operator, operator)
+
+	assert.Assert(testLiteralExpression(assert, opExp.Right, right))
 
 	return true
 }
